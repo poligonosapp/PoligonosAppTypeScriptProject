639955c596ae18f8a131568ea1f3eb15a9b62dd0
Luis Mendes <machadoluismendes@gmail.com>
2022-12-23 08:50:54 -0300

    Increment
    Hermes engine

--- PoligonosAppTypeScriptProject/src/map/map.tsx
+++ PoligonosAppTypeScriptProject/src/map/map.tsx
@@ -6,7 +6,7 @@ import { AppState, AppAction } from '../interfaces';
 import container from '../inversify.config';
 
 // Inject the store and any other dependencies
-const store = container.get<Store<AppState, AppAction>>('Store');
+const store = container.get<Store<AppState, AppAction>('Store');
 // const someService = container.get<SomeService>('SomeService');
 
 // Use the store and dependencies in the module's components
