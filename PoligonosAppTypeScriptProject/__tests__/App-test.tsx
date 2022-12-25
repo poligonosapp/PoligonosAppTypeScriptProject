e4c86ce4ecdf2e6b2481c01f20ed5fa1f94c4452
Luis Mendes <machadoluismendes@gmail.com>
2022-12-25 20:06:30 -0300

    Increment
    yarn jest stack babel webpack lodash jsdom jest-runner jest-worker whatwg-url

--- PoligonosAppTypeScriptProject/__tests__/App-test.tsx
+++ PoligonosAppTypeScriptProject/__tests__/App-test.tsx
@@ -9,6 +9,17 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const _ = require("lodash");
+
 it('renders correctly', () => {
+
+try {
+  _._baseFlatten;
+} catch (e) {
+  console.log("App-test.tsx Cannot find module './_baseFlatten'")
+} finally {
+  
+}
+
   renderer.create(<App />);
 });
