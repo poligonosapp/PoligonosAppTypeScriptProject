a60e88b7ec01101edb2c6da43fa24a40b535c23a
Luis Mendes <machadoluismendes@gmail.com>
2022-12-26 14:41:47 -0300

    Increment
    yarn add @types/jest

--- PoligonosAppTypeScriptProject/__tests__/App-test.tsx
+++ PoligonosAppTypeScriptProject/__tests__/App-test.tsx
@@ -9,10 +9,12 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const sortBy = require('lodash.sortby');
+
 it('renders correctly', () => {
 
   try {
-  const _ = require("lodash");
+  const _ = require("lodash");// downgrade to lodash@4.17.20
   _._baseFlatten();
 } catch (e) {
   console.log("App-test.tsx Cannot find module './_baseFlatten'");
