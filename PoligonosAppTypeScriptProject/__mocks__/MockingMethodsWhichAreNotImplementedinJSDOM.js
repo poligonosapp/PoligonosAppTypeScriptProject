687bc96191da05269d895dfc79ac85a44a8cc7fe
Luis Mendes <machadoluismendes@gmail.com>
2022-12-26 15:01:26 -0300

    Increment
    https://jestjs.io/docs/manual-mocks#mocking-node-modules

--- PoligonosAppTypeScriptProject/__mocks__/MockingMethodsWhichAreNotImplementedinJSDOM.js
+++ PoligonosAppTypeScriptProject/__mocks__/MockingMethodsWhichAreNotImplementedinJSDOM.js
@@ -1,4 +1,4 @@
-// jest lodash 
+// jest lodash https://jestjs.io/docs/manual-mocks#mocking-node-modules
 import './matchMedia.mock'; // Must be imported before the tested file
 import {myMethod} from './file-to-test';
 
